7b037999dbbfed7b6fd90f46e92db7cfdd9450bd
Lukas Benedix <lbenedix@users.noreply.github.com>
2015-05-31 22:22:54 +0200

    deleted unnecessary stuff

--- test_rotary_class.py
+++ test_rotary_class.py
@@ -1,7 +1,6 @@
 import sys
 import time
 from rotary_class import RotaryEncoder
-import cv2
 
 
 # Define GPIO inputs
@@ -14,13 +13,8 @@ BUTTON = 4	# Pin 7
 # This is the event callback routine to handle events
 def showImage(event, frame):
 	if event == RotaryEncoder.CLOCKWISE:
-		image = 'karneval/test%04d.jpg' % (frame,)
 		print frame, "Clockwise", image
 
-		img = cv2.imread(image,0)
-		cv2.imshow('image',img)
-		cv2.waitKey(0)
-		cv2.destroyAllWindows()
 	elif event == RotaryEncoder.ANTICLOCKWISE:
 		print frame, "Anticlockwise"
 	
