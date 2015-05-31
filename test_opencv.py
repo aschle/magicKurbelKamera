6ebc31e3cea790a4eafa28aa2e63cc058397c2b9
Alexa Schlegel <alexa.schlegel@inf.fu-berlin.de>
2015-05-31 22:52:34 +0200

    ups

--- test_opencv.py
+++ test_opencv.py
@@ -5,21 +5,16 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN  )
 
-image1 = pygame.image.load("video/frames/NEUBR_24_0001.jpg")
-image2 = pygame.image.load("video/frames/NEUBR_24_0100.jpg")
-
 clock = pygame.time.Clock()
 
 count = 1
 
 while 1:
 
-		for event in pygame.event.get():
-
-				image = 'video/frames/NEUBR_24_%04d.jpg' % (count,)
-				img = pygame.image.load(image)
-				screen.fill((255,255,255))
-				screen.blit(img,(0,0))
-				clock.tick(1)
-				pygame.display.flip()
-				count = count + 1
+		image = 'video/frames/NEUBR_24_%04d.jpg' % (count,)
+		img = pygame.image.load(image)
+		screen.fill((255,255,255))
+		screen.blit(img,(0,0))
+		clock.tick(10)
+		pygame.display.flip()
+		count = count + 1
