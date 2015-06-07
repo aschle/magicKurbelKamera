60271aaea40033a777c401ec157f3f5d393735b5
Lukas Benedix <lbenedix@users.noreply.github.com>
2015-06-07 21:27:48 +0200

    Update prepare_system.sh

--- prepare_system.sh
+++ prepare_system.sh
@@ -1,3 +1,12 @@
+sudo apt-get install python-dev libjpeg8-dev
+#find /usr/lib -name libjpeg.so
+#/usr/lib/arm-linux-gnueabihf/libjpeg.so
+#sudo ln -s /usr/lib/arm-linux-gnueabihf/libjpeg.so /usr/lib/
+#sudo pip install PIL
+
+
+
+
 sudo mkdir /var/tmp
 echo "tmpfs /var/tmp tmpfs nodev,nosuid,size=250M 0 0" | sudo tee --append /etc/fstab
 sudo mount -a
