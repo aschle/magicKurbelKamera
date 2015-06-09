9e1515e0c4aee2c792a650d7698f2028cf989e4c
lbenedix <lukas.benedix@fu-berlin.de>
2015-06-09 08:00:59 +0000

    added dropbox and qrcode lib

--- prepare_system.sh
+++ prepare_system.sh
@@ -1,12 +1,23 @@
-sudo apt-get install python-dev libjpeg8-dev
+# sudo apt-get install python-dev libjpeg8-dev
 #find /usr/lib -name libjpeg.so
 #/usr/lib/arm-linux-gnueabihf/libjpeg.so
 #sudo ln -s /usr/lib/arm-linux-gnueabihf/libjpeg.so /usr/lib/
 #sudo pip install PIL
 
 
+# install pip
+sudo apt-get -y install python-pip
 
 
+# dropbox lib for python
+sudo apt-get -y install python-dev
+sudo pip install dropbox
+
+
+# qr lib
+sudo pip install pyqrcode pypng
+
+# create ramdisk
 sudo mkdir /var/tmp
 echo "tmpfs /var/tmp tmpfs nodev,nosuid,size=250M 0 0" | sudo tee --append /etc/fstab
 sudo mount -a
