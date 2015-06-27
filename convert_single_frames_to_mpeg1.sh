e21ddf991d407f426d6a5cfc9c81930fa178942a
Alexa Schlegel <alexa.schlegel@gmail.com>
2015-06-27 12:19:06 +0200

    added convertig ffmpeg befehl

--- convert_single_frames_to_mpeg1.sh
+++ convert_single_frames_to_mpeg1.sh
@@ -1,1 +1,4 @@
 ffmpeg -y -framerate 25 -i frames/Start_%5d.png -vcodec mpeg1video -mbd rd -trellis 2 -cmp 2 -subcmp 2 -g 100 -pass 1 magic.mp4
+
+# convert init video mp4 to mpeg1
+#ffmpeg -y -i init_not_coded.mp4 -vcodec mpeg1video -mbd rd -trellis 2 -cmp 2 -subcmp 2 -g 100 -pass 1 init.mp4
